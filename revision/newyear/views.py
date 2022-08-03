f8c8b478d506a87498bb1d4d7283bdec47bb0762
Muhammed Oladapo <muhammedoladapo3@gmail.com>
2022-08-03 05:46:51 +0100

    rendered a file

---
+++ revision/newyear/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+
+import datetime #import the calener in order to be able to use it in this code. 
+
+
+# Create your views here.
+
+def index(request, newyear):
+    now = datetime.datetime.now()
+    return render(request, "newyear/index.html", {
+        "newyear": now.month == 1 and now.day == 1
+    })
+"""
+So basically, when the function "datetime..." finishes running and saves the result in the
+variable "now". if the context "now.month" is equal to 1 then the value of the variable "newyear"
+is going true when the template gets access to it.
+
+"""
--- revision/revision/settings.py
+++ revision/revision/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'hello', #This is where we install our appps. This is a must.
+    'newyear',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
