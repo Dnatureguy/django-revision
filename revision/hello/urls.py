87dc939a8a628f685c25f33a89c3b53cb72ef658
Muhammed Oladapo <muhammedoladapo3@gmail.com>
2022-08-03 03:10:24 +0100

    basic templates

--- revision/hello/urls.py
+++ revision/hello/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 #urlpatterns is the list of the allowable urls that can be accessed into.
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("david", views.david, name="david"), #david being in the url string means it is now a url and it can be accessed into.
+    path("greet", views.greet, name="greet"), #<str:name> means any string of name is welcome to acess this route.
+    path("<str:name>", views.half, name="half")
 ]
--- revision/hello/views.py
+++ revision/hello/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse   #for us to use httpresponse, we have
+from django.http import HttpResponse   #for us to use httpresponse, we have to import httpresponse
 from django.shortcuts import render
 
 # Create your views here.
@@ -7,9 +7,22 @@ class HttpResponse¶
 In contrast to HttpRequest objects, which are created automatically by Django, 
 HttpResponse objects are your responsibility. 
 Each view you write is responsible for instantiating, populating, and returning an HttpResponse.
-
+HttpResponse is only used to request for strings and not files.
 The HttpResponse class lives in the django.http module.
-
+Rendering of strings is when HttpResponse comes in. Now to render an entire html file, the use of render functionj is needed.
 """
 def index(request):
-    return HttpResponse("Hello, world!")
+    return render(request, "hello/index.html") #to render an html file, you pass in a request and then an template.
+
+def david(request):
+    return HttpResponse("Hi, david!")
+
+def greet(request, name): #remember a function can have more than two arguments, here "name" is trying to be  placeholder.
+    return HttpResponse(f"Hello, {name.capitalize()}") #now,"name" is added to represent any other string.
+
+def half(request, name):
+    return render(request, "hello/half.html", {
+        "name": name.capitalize()
+    }) #Now, there was an optional third argument in the render function, it being the context.
+       #Context contains the information or variables that i want the template to have access to.
+       #"Name" is the key to the value to capitalize the second argument to request function.
